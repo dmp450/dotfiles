e4c7141c7aab8d65f3ad9ef7727672af47bf8144
Derek Perrin <derek@derekperrin.com>
2021-11-13 12:39:53 -0600

    Add web mode to init.el and reftex plugin for AUCTeX.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -146,7 +146,8 @@
   :config
   (setq TeX-master nil
 	TeX-auto-save t
-	preview-auto-reveal t)
+	preview-auto-reveal t
+	reftex-plug-into-AUCTeX t)
   (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
   (add-hook 'LaTeX-mode-hook 'turn-on-flyspell))
 
@@ -316,3 +317,36 @@
 ;;     :straight (power-mode :type git :host github :repo "elizagamedev/power-mode.el")
 ;;     :init
 ;;     (add-hook 'after-init-hook #'power-mode))
+
+
+(use-package web-mode
+  :mode
+  (
+   ".twig$"
+   ".html?$"
+   ".hbs$"
+   ".vue$"
+   ".blade.php$"
+   )
+  :config
+  (setq
+   web-mode-markup-indent-offset 2
+   web-mode-css-indent-offset 2
+   web-mode-code-indent-offset 2
+   web-mode-style-padding 2
+   web-mode-script-padding 2
+   web-mode-enable-auto-closing t
+   web-mode-enable-auto-opening t
+   web-mode-enable-auto-pairing t
+   web-mode-enable-auto-indentation t)
+
+  ;; Let smartparens handle auto closing brackets, e.g. {{ }} or {% %}
+  ;; https://github.com/hlissner/doom-emacs/blob/develop/modules/lang/web/%2Bhtml.el#L56
+  (dolist (alist web-mode-engines-auto-pairs)
+    (setcdr alist
+            (cl-loop for pair in (cdr alist)
+                     unless (string-match-p "^[a-z-]" (cdr pair))
+                     collect (cons (car pair)
+                                   (string-trim-right (cdr pair)
+                                                      "\\(?:>\\|]\\|}\\)+\\'")))))
+  )

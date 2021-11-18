270e6d36873e661723fa46b2e3489ce1639ea9fa
Derek Perrin <derek@derekperrin.com>
2021-11-18 00:46:43 -0600

    Add some basic support for elfeed.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -229,6 +229,30 @@
   :config
   (add-hook 'magit-mode-hook 'xah-fly-insert-mode-activate))
 
+(use-package elfeed
+  :ensure t
+  :config
+  ;; Star and unstar articles
+  ;; http://pragmaticemacs.com/emacs/star-and-unstar-articles-in-elfeed/
+  (defalias 'elfeed-toggle-star
+    (elfeed-expose #'elfeed-search-toggle-all 'star))
+
+  (eval-after-load 'elfeed-search
+    '(define-key elfeed-search-mode-map (kbd "m") 'elfeed-toggle-star))
+
+  (defface elfeed-search-starred-title-face
+    '((t :foreground "#f77"))
+    "Marks a starred Elfeed entry.")
+
+  (push '(starred elfeed-search-starred-title-face) elfeed-search-face-alist))
+
+(use-package elfeed-org
+  :ensure t
+  :config
+  (elfeed-org)
+  (setq rmh-elfeed-org-files (list "~/.emacs.d/feeds.org")))
+
+
 ;; My mail configuration
 ;; TODO: Look at adding in folding perhaps?
 (use-package mu4e
